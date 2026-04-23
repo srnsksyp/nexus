797bcc7ac17c9bcbb8fdcc29fa77ecc364794ef7
Saransh Kashyap <saranshk0511@gmail.com>
2026-04-23 22:28:44 +0530

    02: Add Clerk authentication integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  Show,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import { dark } from "@clerk/themes";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -27,19 +38,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      theme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <SignedOut>
+                <SignInButton>
+                  <button className="bg-blue-500 text-white p-2 rounded">
+                    Sign In
+                  </button>
+                </SignInButton>
+
+                <SignUpButton>
+                  <button className="bg-rose-500 text-white p-2 rounded">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
