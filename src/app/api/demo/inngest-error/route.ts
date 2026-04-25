05fdea8f2f8217631cf386b57a2d927e25e02a4f
Saransh Kashyap <saranshk0511@gmail.com>
2026-04-25 15:56:29 +0530

    07: add authentication verification and project management features

--- src/app/api/demo/inngest-error/route.ts
+++
@@ -1,9 +0,0 @@
-import { inngest } from "@/inngest/client";
-
-export async function POST() {
-  await inngest.send({ name: "demo/error", data: {} });
-
-  return Response.json({
-    status: "Started background job that will throw an error",
-  });
-}
